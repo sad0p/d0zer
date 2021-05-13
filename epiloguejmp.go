75b6861741c0992b9c7e3ae917ba321c07eb8c2a
sad0p <sad0p@protonmail.com>
2021-05-13 04:58:17 -0400

    removed commented out code

--- epiloguejmp.go
+++ epiloguejmp.go
@@ -17,55 +17,6 @@ func modEpilogue64(pSize int32, pEntry uint64, oEntry uint64) []byte {
 		0x48, 0x8b, 0x04, 0x24, 			//mov    (%rsp),%rax
 		0xc3, 								//ret
 	}
-
-	fmt.Println("Shellcode before")
-	for _, v := range epilog {
-		fmt.Printf("%02x ", v)
-	}
-	encpSize := make([]byte, 4)   
-
-	var noZeros int //does not count bytes of value 0x00 associated with extension
-	var incOff int = 0x12
-
-	var boolean adjustSize
-	if ! (pSize <= 0xff) {
-		binary.LittleEndian.PutUint32(encpSize, uint32(pSize))
-		fmt.Printf("%2x\n", encpSize)
-		for _, b := range encpSize {
-			fmt.Printf("%02x\n", b)
-			if b != 0x00 {
-				noZeros++
-			}
-		}
-		incOff += (noZeros - 1) //increase the relative call offset in the beginning of the epilog shellcode
-		adjustSize =  true
-	}
-	fmt.Printf("pSize = %d | pEntry = %d | oEntry = %d\n", pSize, pEntry, oEntry)
-	fmt.Println("Num of zeros in pSize ", (noZeros - 4))
-
-	for i := 0; i < 4; i++ {
-		fmt.Printf("%x\n", encpSize[i])
-	}
-
-	//fmt.Printf("0x%x\n", encpSize[:noZeros])
-	if incOff != 0 { 
-		binary.LittleEndian.PutUint32(epilog[x64_STUB_CALL_START:], uint32(incOff))
-	}
-	
-	fmt.Println("Shellcode after")
-	for _, v := range epilog {
-		fmt.Printf("%02x ", v)
-	}
-
-	var modEpilog bytes.Buffer
-	modEpilog.Write(epilog[:x64_STUB_CALL_END])
-	if adjustSize {
-		//write sub rax
-		modEpilog.Write(epilog[5:9])
-		//write 2nd operand
-		for(i := 9; i < 13; i++) {
-		}
-	}
 	*/
 	var incOff uint32 = 0x12
 	encPsize := make([]byte, 4)
@@ -93,7 +44,7 @@ func modEpilogue64(pSize int32, pEntry uint64, oEntry uint64) []byte {
 
 	//write sub rax, encPsize
 	shellcode.Write([]byte{0x48, 0x83, 0xe8})
-	shellcode.Write(encPsize)
+	shellcode.Write(encPsize[:numZeros])
 
 	//write sub rax, pEntry
 	encPentry := make([]byte, 4)

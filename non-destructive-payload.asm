32031ea662d758d8da887b52a5b723b2c6c2bd96
sad0p <sad0p@protonmail.com>
2021-05-22 19:09:34 -0400

    removing ret2OEP stub example non-destructive payload

--- non-destructive-payload.asm
+++ non-destructive-payload.asm
@@ -16,7 +16,7 @@ shellcode:
 	mov rax, 0x1 		;write system call
 	mov rdi, 0x1      	;stdout fd
 
-	pop rsi			;make rsi a ptr to message
+	pop rsi				;make rsi a ptr to message
 	mov rdx, 0x2a 		;message length
 	syscall			
 
@@ -27,18 +27,6 @@ shellcode:
 	pop rdx
 	pop rsi
 	pop rdi
-
-	call get_eip
-	sub rax, 0x04f ; size of virus code
-	sub rax, 0x173d1 ; textSegment.virtualAddr + textSegment.FileSZ AKA end of textSegment original code and start of vxcode, rax is now the base virtual addr
-	add rax, 0x5b20 ; OEP
-	jmp rax
-
-
-
-get_eip:
-	mov rax, [rsp]
-	ret
 
 
 

